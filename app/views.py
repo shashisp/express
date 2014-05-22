f456e47168d1196a8adeaf14e5b2d164818ac7e4
Shashi Essp <shashiessp@gmail.com>
2014-05-22 20:39:02 +0530

    added templates

--- app/views.py
+++ app/views.py
@@ -1,9 +1,24 @@
 from app import app
+from flask import render_template 
 
 @app.route('/')
 
 @app.route('/index')
 
 def index():
-	return "test"
-	
+	user = { 'name' : 'Essp'}
+	posts = [ # fake array of posts
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+
+	return render_template("index.html",
+		title = "Home",
+		user = user,
+		posts = posts)
